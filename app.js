6e18f169e0344a30db8b6cdf45a01e0f40cb08b0
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-03-12 15:32:59 -0500

    added projects and link to shuffling cards. not mobile ready yet

--- app.js
+++ app.js
@@ -1,47 +1,106 @@
-// const projectName = document.querySelector('project-name');
-// const projectDescription = document.querySelector('project-description');
-// const projectLink = document.querySelector('project-href');
-// const projectCard = document.querySelector('card');
+const nameProject = document.querySelector('.project-name');
+const projectDescription = document.querySelector('.project-description');
+const projectLink = document.querySelector('.project-href');
+const projectCard = document.getElementById('arrow-container');
 
-// const projects = [
-//   {
-//     name: 'First attempt at personal page.',
-//     description: 'Simple page with basic HTML & CSS',
-//     link: 'https://newbie814.github.io/cv-update/',
-//   },
-//   {
-//     name: 'Commercial sales page',
-//     description:
-//       'HTML, CSS, Bootstrap. I was following a tutorial. This was a challenge, as a lot of the technologies used were deprecated. This actually taught me what deprecated was. I had to research a lot for workarounds.',
-//     link: 'https://newbie814.github.io/tindog-lesson2/',
-//   },
-//   {
-//     name: 'High Roll Wins.',
-//     description:
-//       'Simple game based on a random number generator in Javascript. Simple. But, my 3 year old loved it.',
-//     link: 'https://newbie814.github.io/mama_connor_dice/',
-//   },
-//   {
-//     name: "Connor's Drum Kit",
-//     description: 'Practice with DOM, event listeners, and switch/case',
-//     link: 'https://newbie814.github.io/udemy-drumkit-project/',
-//   },
-// ];
+const projects = [
+  {
+    name: 'First attempt at personal page.',
+    description: 'Simple page with basic HTML & CSS',
+    link: 'https://newbie814.github.io/cv-update/',
+  },
+  {
+    name: 'Commercial sales page',
+    description:
+      'HTML, CSS, Bootstrap. I was following a tutorial. This was a challenge, as a lot of the technologies used were deprecated. I had to research a lot for workarounds.',
+    link: 'https://newbie814.github.io/tindog-lesson2/',
+  },
+  {
+    name: 'High Roll Wins.',
+    description:
+      'Simple game based on a random number generator in Javascript. Simple. But, my 3 year old loved it.',
+    link: 'https://newbie814.github.io/mama_connor_dice/',
+  },
+  {
+    name: "Connor's Drum Kit",
+    description: 'Practice with DOM, event listeners, and switch/case',
+    link: 'https://newbie814.github.io/udemy-drumkit-project/',
+  },
+  {
+    name: 'Simon Game',
+    description:
+      'More practice with event listeners, arrays, and array methods.',
+    link: 'https://newbie814.github.io/simon-game-challenge/',
+  },
+  {
+    name: 'Father and son',
+    description: 'Hidden search widget. ish.',
+    link: ' https://newbie814.github.io/hiddenish-search-widget/',
+  },
+  {
+    name: 'Expanding photo cards',
+    description:
+      'More event listeners. Learning to use JavaScript to add and remove classes',
+    link: 'https://newbie814.github.io/exp-attempt-3/',
+  },
+  {
+    name: 'Lil Dude changing a tire',
+    description: 'Full photo background with blur to focus animation',
+    link: 'https://github.com/Newbie814/blur-load/settings/pages',
+  },
+  {
+    name: 'Carousel carouselling',
+    description: 'Spinning and separating photo animation',
+    link: 'https://newbie814.github.io/connor-carousel-3d-squares/',
+  },
+  {
+    name: 'MJ Thomas Art E-commerce site',
+    description:
+      'Fully functioning. Pay system is in sandbox mode. Built with HTML, CSS, React, Bootstrap, Redux, NodeJS, JavaScript MongoDB, Mongoose, Atlas, Postman, PayPal API. Deployed and hosted on Heroku.',
+    link: 'https://mjthomasart.herokuapp.com/',
+  },
+  {
+    name: 'Portfolio',
+    description:
+      'Built on Codesandbox. Deployed via CS through Netlify. Domain is from Google.',
+    link: 'http://www.matthew-woodard.com/',
+  },
+  {
+    name: 'Verification Code',
+    description: 'Simple UI screen for inputting 2FA code',
+    link: 'https://newbie814.github.io/2fa-ui-screen/',
+  },
+  {
+    name: 'Search Filter',
+    description: 'Simple filter to search users',
+    link: 'https://newbie814.github.io/live-feed-user-filter',
+  },
+  {
+    name: 'Personal Training Website',
+    description: "My own personal site. It's a work in progress.",
+    link: 'https://newbie814.github.io/personal-training-page/',
+  },
+];
 
-// let index = 1;
+let index = 1;
 
-// function flipThroughCards() {
-//   const { name, description, link } = projects[index];
+// function flipThroughCards()
 
-//   projectName.innerHTML = name;
-//   projectDescription.innerHTML = description;
-//   // projectLink.getAttribute('href') = link;
+document
+  .getElementById('arrow-container')
+  .addEventListener('click', function () {
+    {
+      const { name, description, link } = projects[index];
 
-//   index++;
+      nameProject.innerHTML = name;
+      projectDescription.innerHTML = description;
+      projectLink.innerHTML = link;
+      projectLink.setAttribute('href', 'link');
 
-//   if (index > projects.length - 1) {
-//     index = 0;
-//   }
-// }
+      index++;
 
-// projectName.addEventListener('click', flipThroughCards);
+      if (index > projects.length - 1) {
+        index = 0;
+      }
+    }
+  });
