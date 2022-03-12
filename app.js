e7421d383c48268dca36157cd397d71ec7871478
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-03-12 00:51:16 -0500

    added ttf font. styled cards. added background image (card  and body)

---
+++ app.js
@@ -0,0 +1,47 @@
+// const projectName = document.querySelector('project-name');
+// const projectDescription = document.querySelector('project-description');
+// const projectLink = document.querySelector('project-href');
+// const projectCard = document.querySelector('card');
+
+// const projects = [
+//   {
+//     name: 'First attempt at personal page.',
+//     description: 'Simple page with basic HTML & CSS',
+//     link: 'https://newbie814.github.io/cv-update/',
+//   },
+//   {
+//     name: 'Commercial sales page',
+//     description:
+//       'HTML, CSS, Bootstrap. I was following a tutorial. This was a challenge, as a lot of the technologies used were deprecated. This actually taught me what deprecated was. I had to research a lot for workarounds.',
+//     link: 'https://newbie814.github.io/tindog-lesson2/',
+//   },
+//   {
+//     name: 'High Roll Wins.',
+//     description:
+//       'Simple game based on a random number generator in Javascript. Simple. But, my 3 year old loved it.',
+//     link: 'https://newbie814.github.io/mama_connor_dice/',
+//   },
+//   {
+//     name: "Connor's Drum Kit",
+//     description: 'Practice with DOM, event listeners, and switch/case',
+//     link: 'https://newbie814.github.io/udemy-drumkit-project/',
+//   },
+// ];
+
+// let index = 1;
+
+// function flipThroughCards() {
+//   const { name, description, link } = projects[index];
+
+//   projectName.innerHTML = name;
+//   projectDescription.innerHTML = description;
+//   // projectLink.getAttribute('href') = link;
+
+//   index++;
+
+//   if (index > projects.length - 1) {
+//     index = 0;
+//   }
+// }
+
+// projectName.addEventListener('click', flipThroughCards);
